3dd1965bc6294ebf8864e9ca2b0a51c1cc21ad45
Clemens <ryuma12000@yahoo.com>
2025-11-10 10:42:49 +0100

    Mina hellow world

--- src/vecka6/Git/HelloWorld3.java
+++ src/vecka6/Git/HelloWorld3.java
@@ -2,6 +2,6 @@ package vecka6.Git;
 
 public class HelloWorld3 {
     static void main(String[] args) {
-        System.out.println("Hello world3");
+        System.out.println("Hello world5 frpm clemens");
     }
 }
